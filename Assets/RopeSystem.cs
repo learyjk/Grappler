7edd47fa4776159e9ad59ddc851174a2596fba0c
Keegan Leary <keeganleary@Keegans-MacBook-Pro.local>
2020-06-16 14:11:45 -0700

    First Commit

---
+++ Assets/RopeSystem.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RopeSystem : MonoBehaviour
+{
+    public GameObject ropeHingeAnchor;
+    public DistanceJoint2D ropeJoint;
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    private bool ropeAttached;
+    private Vector2 playerPosition;
+    private Rigidbody2D ropeHingeAnchorRb;
+    private SpriteRenderer ropeHingeAnchorSprite;
+
+    public LineRenderer ropeRenderer;
+    public LayerMask ropeLayerMask;
+    private float ropeMaxCastDistance = 20f;
+    private List<Vector2> ropePositions = new List<Vector2>();
+    private bool distanceSet;
+
+    private void Awake() 
+    {
+        ropeJoint.enabled = false;
+        playerPosition = transform.position;
+        ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirectioin = (worldMousePosition - transform.position).normalized;
+        var aimAngle = Mathf.Atan2(facingDirectioin.y, facingDirectioin.x);
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        playerPosition = transform.position;
+
+        if (!ropeAttached)
+        {
+            SetCroshairPosition(aimAngle);
+        }
+        else
+        {
+            crosshairSprite.enabled = false;
+        }
+
+        HandleInput(aimDirection);
+        UpdateRopePositions();
+    }
+
+    private void SetCroshairPosition(float aimAngle)
+    {
+        if (!crosshairSprite.enabled)
+        {
+            crosshairSprite.enabled = true;
+        }
+
+        var x = transform.position.x + 1f * Mathf.Cos(aimAngle);
+        var y = transform.position.y + 1f * Mathf.Sin(aimAngle);
+
+        var crosshairPosition = new Vector3(x, y, 0);
+        crosshair.transform.position = crosshairPosition;
+    }
+
+    private void HandleInput(Vector2 aimDirection)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (ropeAttached) return;
+            ropeRenderer.enabled = true;
+
+            var hit = Physics2D.Raycast(playerPosition, aimDirection, ropeMaxCastDistance, ropeLayerMask);
+
+            if (hit.collider != null)
+            {
+                ropeAttached = true;
+                if (!ropePositions.Contains(hit.point))
+                {
+                    //Jump slightly to distance the player from ground after grappling
+                    transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 2f), ForceMode2D.Impulse);
+                    ropePositions.Add(hit.point);
+                    ropeJoint.distance = Vector2.Distance(playerPosition, hit.point);
+                    ropeJoint.enabled = true;
+                    ropeHingeAnchorSprite.enabled = true;
+                }
+            }
+            else
+            {
+                ropeRenderer.enabled = false;
+                ropeAttached = false;
+                ropeJoint.enabled = false;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            ResetRope();
+        }
+    }
+
+    private void ResetRope()
+    {
+        ropeJoint.enabled = false;
+        ropeAttached = false;
+        //playerMovement.isSwinging = false;
+        ropeRenderer.positionCount = 2;
+        ropeRenderer.SetPosition(0, transform.position);
+        ropeRenderer.SetPosition(1, transform.position);
+        ropePositions.Clear();
+        ropeHingeAnchorSprite.enabled = false;
+    }
+
+    private void UpdateRopePositions()
+{
+    // return if no rope attached.
+    if (!ropeAttached)
+    {
+        return;
+    }
+
+    // set vertex count +1 for player position
+    ropeRenderer.positionCount = ropePositions.Count + 1;
+
+    // set vertex position for each vertex in ropePositions
+    for (var i = ropeRenderer.positionCount - 1; i >= 0; i--)
+    {
+        if (i != ropeRenderer.positionCount - 1) // if not the Last point of line renderer
+        {
+            ropeRenderer.SetPosition(i, ropePositions[i]);
+                
+            // set rope anchor
+            if (i == ropePositions.Count - 1 || ropePositions.Count == 1)
+            {
+                var ropePosition = ropePositions[ropePositions.Count - 1];
+                if (ropePositions.Count == 1)
+                {
+                    ropeHingeAnchorRb.transform.position = ropePosition;
+                    if (!distanceSet)
+                    {
+                        ropeJoint.distance = Vector2.Distance(transform.position, ropePosition);
+                        distanceSet = true;
+                    }
+                }
+                else
+                {
+                    ropeHingeAnchorRb.transform.position = ropePosition;
+                    if (!distanceSet)
+                    {
+                        ropeJoint.distance = Vector2.Distance(transform.position, ropePosition);
+                        distanceSet = true;
+                    }
+                }
+            }
+            // handle case where we are at second-to-last vertex which will be the hinge point.
+            else if (i - 1 == ropePositions.IndexOf(ropePositions.Last()))
+            {
+                var ropePosition = ropePositions.Last();
+                ropeHingeAnchorRb.transform.position = ropePosition;
+                if (!distanceSet)
+                {
+                    ropeJoint.distance = Vector2.Distance(transform.position, ropePosition);
+                    distanceSet = true;
+                }
+            }
+        }
+        else
+        {
+            // set last rope vertex to player's position.
+            ropeRenderer.SetPosition(i, transform.position);
+        }
+    }
+}
+
+}
